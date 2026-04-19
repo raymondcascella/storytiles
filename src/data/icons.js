11205534afd1a5d4a56b987fbc4b6a8b3168a7b6
Raymond Cascella <raymondcascella@gmail.com>
2026-04-19 10:57:13 -0400

    feat: add curated FA icon list

---
+++ src/data/icons.js
@@ -0,0 +1,21 @@
+import {
+  faHouse, faStar, faHeart, faBolt, faTree,
+  faCar, faPlane, faShip, faBicycle, faRocket,
+  faDog, faCat, faDragon, faFish,
+  faAppleWhole, faMugHot, faPizzaSlice, faIceCream, faCakeCandles,
+  faBook, faMusic, faCamera, faGem, faTrophy,
+  faSun, faMoon, faCloud, faSnowflake, faUmbrella,
+  faFire, faLeaf, faSeedling, faMountain,
+  faFaceSmile, faFaceAngry, faFaceSurprise, faFaceSadTear, faThumbsUp,
+} from '@fortawesome/free-solid-svg-icons'
+
+export const ICONS = [
+  faHouse, faStar, faHeart, faBolt, faTree,
+  faCar, faPlane, faShip, faBicycle, faRocket,
+  faDog, faCat, faDragon, faFish,
+  faAppleWhole, faMugHot, faPizzaSlice, faIceCream, faCakeCandles,
+  faBook, faMusic, faCamera, faGem, faTrophy,
+  faSun, faMoon, faCloud, faSnowflake, faUmbrella,
+  faFire, faLeaf, faSeedling, faMountain,
+  faFaceSmile, faFaceAngry, faFaceSurprise, faFaceSadTear, faThumbsUp,
+]
